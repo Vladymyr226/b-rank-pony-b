33e3fe76eacb436ef8b525ec42a14a0092286fda
ArtemDid <temadidenko27@gmail.com>
2024-06-24 21:38:03 +0300

    added db

--- migrations/20240624151256_users.js
+++ migrations/20240624151256_users.js
@@ -2,7 +2,8 @@ exports.up = async function (knex) {
   return knex.schema.createTable('users', function (table) {
     table.increments('id').primary()
     table.integer('user_tg_id').notNullable()
-    table.string('phone_number', 15).notNullable()
+    table.string('username', 255).notNullable()
+    table.string('phone_number', 15).nullable()
     table.timestamp('created_at').defaultTo(knex.fn.now())
     table.timestamp('updated_at').defaultTo(knex.fn.now())
   })
---
+++ src/modules/bot/bot.types.ts
@@ -0,0 +1,10 @@
+export type TUser = {
+  id?: number
+  user_tg_id: number
+  username: string
+  phone_number?: string
+  first_name?: string
+  last_name?: string
+  created_at?: Date
+  updated_at?: Date
+}
