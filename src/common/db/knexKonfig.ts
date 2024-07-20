d14f87234fa7b7b002abea2d41e5d6e3e3b87bcc
ArtemDid <temadidenko27@gmail.com>
2024-07-20 17:03:47 +0300

    fixed heroku db

--- src/common/db/knexKonfig.ts
+++ src/common/db/knexKonfig.ts
@@ -1,10 +1,13 @@
 import knex, { Knex } from 'knex'
 
-const { PG_CONNECTION_STRING } = process.env
+const { DB_HOST, DB_PORT, DB_NAME, DB_USERNAME, DB_PASSWORD, DATABASE_URL } = process.env
 
 export const getKnexConfig = () => ({
   client: 'pg',
-  connection: PG_CONNECTION_STRING,
+  connection: {
+    connectionString: DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
+  },
   migrations: {
     tableName: 'migrations',
     extension: 'js',
