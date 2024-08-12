e38dbb94a2d8252d46731a78ed17955d8e4e59b8
ArtemDid <temadidenko27@gmail.com>
2024-08-12 13:04:51 +0300

    added log in chat

--- src/modules/salon/salon.controller.ts
+++ src/modules/salon/salon.controller.ts
@@ -1,9 +1,23 @@
 import { Request, Response } from 'express'
 import { salonRepository } from './salon.repository'
+import getBotInstance from '../common/bot'
+import { getLogger } from '../../common/logging'
+
+const log = getLogger()
+const bot = getBotInstance()
 
 export const registrationHandler = async (req: Request, res: Response) => {
   const order = await salonRepository.insertOrder(req.body)
 
-  if (order.length) res.status(200).json({ status: 'OK' })
-  else res.status(400).json({ details: [{ message: `Виникла помилка. Спробуйте знову.` }] })
+  if (order.length) {
+    await bot.sendMessage(
+      '-4225519854',
+      `New order: 
+id: ${order[0].id}`,
+    )
+
+    log.info(`New order: id: ${order[0].id}`)
+
+    res.status(200).json({ status: 'OK' })
+  } else res.status(400).json({ details: [{ message: `Виникла помилка. Спробуйте знову.` }] })
 }
